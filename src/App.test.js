339ca1aaddf144bee4d586c1dfb4f9637ce74d54
Brandon Walden <brandonshanewalden@gmail.com>
2017-09-21 11:15:21 -0400

    testing passed

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, mount, render } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -15,9 +16,9 @@ describe('<App />', () => {
     })
 
     test('it increments counter on button click', () => {
-        const component = mount(<App />);
-        component.find(".btn-increment")// finish this statement using "simulate" and "expect" to test if the increment button was pressed and the score is 1.
-
+        const wrapper = mount(<App />);
+        wrapper.find(".btn-increment").simulate('click');// finish this statement using "simulate" and "expect" to test if the increment button was pressed and the score is 1.
+        expect(wrapper.state('score')).toEqual(1)
 
         // test what happens if the decrement button is pressed....
         // hint: it should decrease to 0 if increment was just pressed.
@@ -30,6 +31,8 @@ describe('<App />', () => {
         component.setState({score: 0});
         expect(component.instance().scoreColor()).toEqual('red');
 
-    // test if the color will be green if it is a positive integer...
+        // test if the color will be green if it is a positive integer...
+        component.setState({score: 1});
+        expect(component.instance().scoreColor()).toEqual('green');
     })
 })
